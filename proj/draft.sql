7ded9952099faf3e2bf563900747bfaeeaaf4978
NancyZhang <Nancy.Zhang@fisherpaykel.com>
2022-10-06 17:42:59 +1300

    ssss

---
+++ proj/draft.sql
@@ -0,0 +1,300 @@
+S.M.A.R.T. Short term (6 - 12 months)
+
+goal: 
+I want to Identify core skill set that I want to grow into and develop domain expertise in (Python)
+
+I want to develop proficiency in integration 
+by training and being involved in client projects 
+
+or demonstrate skills 
+by completing a mock integration project using preferred technical stack + cloud
+
+           Description:
+In my previous working company, I have already got lots of experience on python, 
+so it's easier to work with python than any other programming language, 
+I can develop my skill further based on the current knowledge and experience without start from scratch. 
+
+And there’re lots of powerful cloud sdk python library, 
+so it would definitely useful for the business of my current department.
+
+        I will demo the progress by the following way:   
+I would put python on the top of the candidate list 
+when I have to do the coding to solve the client’s problem when it is possible. 
+
+·    Description:
+Although I have some experience in python, I don’t have much experience of using python with cloud. 
+To master a skill, it is always important to choose the correct way at the beginning and strengthen the skill 
+through sufficient exercise. 
+So I think It would be helpful to take some relevant training, 
+have some particular practice, and further get some the certificate for applying python + cloud knowledge. 
+
+              I will demo the progress by the following way:   
+involved in client projects / demonstrate skills 
+by completing a mock integration project using preferred technical stack + cloud
+（I will take some aws trainings and use sandbox account to practice pupular skills like lambda, iam, cloudwatch.）
+Get 1-2 certificate
+
+1708
+
+Milestone:
+AZ-900 contains lots of important concept for Azure beginners, so the exam should be passed ASAP, ideally in the end of July, no later than Aug.
+The first version of the demo should be released in the coming Oct,
+then refine and fix bugs by the end of this year.
+
+· Deadline:
+    Aug 2022 – pass the AZ 900 exam (completed)
+    Sep 2022 – Online traing:Building Modern Python Applications on AWS and 
+Oct 2022 - Small project demo
+Medium term (12 - 18 months)
+
+S.M.A.R.T. Medium term (12 - 18 months)
+goal: Master AWS Developer skills by passing AWS Certified Developer Associate 
+by end of 2022 and Master skills on chosen domain (based on short term goal)
+
+·  Description:
+AWS is in leading position in the cloud market. And there're lots of clients considering using this platform. 
+So I think learning some AWS skills is also very useful. 
+I will take some more aws develop trainings and prepare for the AWS Certified Developer Associate Certificate.
+
+I will demo the progress by the following ways:
+            Involved in client projects;
+         	Pass the AWS Certified Developer Associate exam.
+
+goal:  Improve communication skills
+
+·  Description:
+In the consulting company, we always have to face the client directly, 
+so it’s extremely to have effective communication to avoid misunderstanding and save the communication cost. 
+I will try to expand my technical vocabulary, 
+be sensitive to the key technical word to understand my colleagues and clients quickly and accurately. 
+
+And also, I will use the correct grammar to express myself without misleading other people. 
+At the same time, I will use some way to help me memories the communication summary, 
+for example, I will take notes in meetings to avoid missing important event.
+
+I will demo the progress by the following way:
+Create a technical new wordlist and maintain them regularly;
+Store the meeting notes locally and refer to them while carry out the tasks when it is necessary. 
+
+·    Milestone:
+I have just chosen the cloud preference  and 
+have not started the preparation for the AWS Certified Developer Associate exam, 
+so there may be some risk in passing the exam in time. 
+Ideally by end of 2022, no later than end of FY23.
+
+· Deadline:
+           June 2023 – pass AWS Certified Developer Associate exam
+           June 2023 - master simple usage of aws and apply them in projects
+            Applied them in 463334 CON - Integration Platform Delivery (Phase 1) ANDO INSURANCE GROUP LIMITED
+
+
+
+
+
+backtrace:
+
+while(idx < arr_len):
+    idx += 1
+    if idx<arr_len:
+        temp_res = temp_res + input_arr[idx]
+
+    else:
+        idx = input_arr.find(temp_res.pop())
+    
+
+    def isValidSudoku(self, board):
+    seen = sum(
+        (
+            [(c, i), (j, c), (i/3, j/3, c)]
+                for i, row in enumerate(board)
+                    for j, c in enumerate(row)
+                if c != '.'), 
+                []
+                )
+    return len(seen) == len(set(seen))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+aws course
+
+1414
+res = []
+def ssss(k):
+    if ????
+        res = [cur] + res
+    for xxx:
+        ssss(k+1)
+    
+
+1414
+res_arr = []
+def ssss(res):
+    if len(res) == len(nums)
+        res_arr.append(res)
+
+    for num in nums:
+        if num not in res:
+            ssss([num]+res)
+
+1431
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res_arr = []
+        def dfs(res):
+            if len(res) == len(nums):
+                res_arr.append(res)
+                return
+
+            for num in nums:
+                if num not in res:
+                    dfs([num]+res)
+                    
+        dfs([])
+        return res_arr
+
+    
+ttps://mybucketnancy.s3.amazonaws.com/dragonsapp/index.html#/
+id_token=
+
+eyJraWQiOiJSeVdXNm5kYzZHQzlLSStpN1kwVnR6bHU2TEZJVisySHZ2QnlwRU94Ymk0PSIsImFsZyI6IlJTMjU2In0.eyJhdF9oYXNoIjoiVnhDMjI1R3ZJaEFaNXZWQnJmLVN4ZyIsInN1YiI6ImI3NTUzNTFjLTdmNDctNGNmMi05YzFjLWFkYjJjNzFmNWFhNSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9vVmgwTWdnTjIiLCJjb2duaXRvOnVzZXJuYW1lIjoiaGhqam1tMTk4MyIsImF1ZCI6IjRzNWVmZGxmb3FzbjVkaWxtODBlN3VoOTQxIiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE2NjA2Mzc0MzAsImV4cCI6MTY2MDY0MTAzMCwiaWF0IjoxNjYwNjM3NDMwLCJqdGkiOiI3MGU2MWU1YS0xYWNkLTQ5ZWEtODc0My04ODA5MjNiOGVlZTIiLCJlbWFpbCI6Imh4eTgyMDcxOEAxNjMuY29tIn0.ms09s5SNrZw66G9wUnZtConnHQNyqQjwuvxWEFdfpv6aoQxMhtRQWThglNRDEM1rCl7mcUBXqXrybgoRe6DvFenoMES7B0hcoz-jUgEn-OeQdK4hPcDqszMtlpjO-ayuIuQaB7_YncEM5RsOrH6mEu8Hmf1ZlFYkmcCfZ9BVnXnPO-EBiAihm86WCuJaPZmrNdcAZwVJ-DHDjkCYyZ0dxuL3jE01bLjjXfrVzEtMjmHLgOpLTPkz4kD8NARmRN9wNyhLOhnYKBR2ihQ7v-vH8XTAQDPBzFt8igw16ZQL77PKQ0U-O9pVNQANuN-cFoNbmaw6oX4dXFiOfTty5iBVCA&access_token=eyJraWQiOiIrRXlcL2xDa1N6VlwvRW9aMEI3R3ZDV3Q0dGtyWjkxS3JDQTMxMmZrNzMyNmM9IiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiJiNzU1MzUxYy03ZjQ3LTRjZjItOWMxYy1hZGIyYzcxZjVhYTUiLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwiYXV0aF90aW1lIjoxNjYwNjM3NDMwLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9vVmgwTWdnTjIiLCJleHAiOjE2NjA2NDEwMzAsImlhdCI6MTY2MDYzNzQzMCwidmVyc2lvbiI6MiwianRpIjoiY2MxOGE4NjUtN2UwMS00YTVmLWE0ODctYmMyZjNiY2NiZDRlIiwiY2xpZW50X2lkIjoiNHM1ZWZkbGZvcXNuNWRpbG04MGU3dWg5NDEiLCJ1c2VybmFtZSI6ImhoamptbTE5ODMifQ.En8SklaRH4eP3AvIkcZSGEp2SMH4TEugL7OIcar4G5U2jEbaahtXjJBgZUZxhT93OZJB_WsjmEgqI0SH3dpWpu8oyT4ioQ1wIiHtgZBYBwe6cbjNjzQSWGbc9_M7LtU0EiBGTKa756ql5j5uS1xRaavOgxVG28bkfbmYzRjU8ZPHaCK-6Eex9FImokRzfTEF9NkB9eTXytlTNZWJ_Mq3cE6gn0aORngqmrQFcuxbjA2dwB3wFVAWBukzOJyGBhv9pXOk_TLYwKuCVX9p2FCqjHmf8263kDNZvFqKzE-Q4tymbf_q7dcllmUjOyfcQzNwdoB62TMxKX65HL4momEXrQ
+
+&expires_in=3600&token_type=Bearer
+
+
+
+ttps://mybucketnancy.s3.amazonaws.com/dragonsapp/index.html#/id_token=
+
+eyJraWQiOiJSeVdXNm5kYzZHQzlLSStpN1kwVnR6bHU2TEZJVisySHZ2QnlwRU94Ymk0PSIsImFsZyI6IlJTMjU2In0.eyJhdF9oYXNoIjoiQk1KUHJ0X0dWQmZvSWg2ZmpkMUFXQSIsInN1YiI6IjkyMGEzODNkLTVmN2MtNDRiNi05OWM1LWVlMTNmYzQ4NjZlMyIsImNvZ25pdG86Z3JvdXBzIjpbIm9uZSJdLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tXC91cy1lYXN0LTFfb1ZoME1nZ04yIiwiY29nbml0bzp1c2VybmFtZSI6Im56aGFuZzYiLCJjb2duaXRvOnJvbGVzIjpbImFybjphd3M6aWFtOjo5NzM3MjE5NDc3NjE6cm9sZVwvYXdzLXNlcnZpY2Utcm9sZVwvb3BzLmFwaWdhdGV3YXkuYW1hem9uYXdzLmNvbVwvQVdTU2VydmljZVJvbGVGb3JBUElHYXRld2F5Il0sImF1ZCI6IjRzNWVmZGxmb3FzbjVkaWxtODBlN3VoOTQxIiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE2NjA2NDEzMjksImV4cCI6MTY2MDY0NDkyOSwiaWF0IjoxNjYwNjQxMzI5LCJqdGkiOiJmOTJkYjBjOS1iODM4LTQxNmMtYjcwNi03MTBmNTllMzU0NWYiLCJlbWFpbCI6Im56aGFuZzZAZGVsb2l0dGUuY28ubnoifQ.rZ-6X_5sGgd7bPEAyU0TIW4QibL2myNUGgsEFncCKQXvtYeKz_rgf9ufJBrT0T82KQmQ8sS0xzlsVCH4bEvzkxnvRviab22nH79D4LqsAq6z7Y3IN8WEkRgVEYAOx6vpXk-zmUXn7eTm7YwKrzSgU9EJkSi3ZEJWc-BYnyLOUY9sJTkZojQ65FpvmTaO899PzXozHn7owcVE3AqIifgUZVIsbB4J_186pX5oDJq7Uingkt64OFmY3LX_-ooPCfqwR_f_TJkKhr2Zp3HGwkjCucCnugROepziBK59s7S4UyvTkTK5huwRXBP0ugnKe-UfGwRhXb9aDeN6SZdZfKRYdg&access_token=eyJraWQiOiIrRXlcL2xDa1N6VlwvRW9aMEI3R3ZDV3Q0dGtyWjkxS3JDQTMxMmZrNzMyNmM9IiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiI5MjBhMzgzZC01ZjdjLTQ0YjYtOTljNS1lZTEzZmM0ODY2ZTMiLCJjb2duaXRvOmdyb3VwcyI6WyJvbmUiXSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tXC91cy1lYXN0LTFfb1ZoME1nZ04yIiwidmVyc2lvbiI6MiwiY2xpZW50X2lkIjoiNHM1ZWZkbGZvcXNuNWRpbG04MGU3dWg5NDEiLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwiYXV0aF90aW1lIjoxNjYwNjQxMzI5LCJleHAiOjE2NjA2NDQ5MjksImlhdCI6MTY2MDY0MTMyOSwianRpIjoiMDAyY2YzZmYtNzM4Ni00YzY2LWIxMTQtNWUyNGU0NjYxMzJkIiwidXNlcm5hbWUiOiJuemhhbmc2In0.uJ9OVO5IvU4yH-IXooEDaCtBUYFu2CqVE1PKE3HLAqxjOAA4Xg7J3EboWgjsht4dxN5ABkSgNK0AR0DYcNzQZNUDrZ8lGnyOZbyAI6gMxaKeYxgEyzIQ1oe36RfKz-dKSDrAFuH2Jxjf_pK1hudnUMSwgO-_4Qw3s95XgLyTASG2TXpkQJ-zIDX1X8cujT2N7dG6jrKry6muSY1EYAEGCHnQBfupxEdzYq3w69OFo1BD5H-bTjqemg_16pTbjNs4ADVg8z3v_kV1gPjNc1pQ-q13Ph7lEQUmuZmkgpXVtQTltOdvtolSpkzb727IXb34NUdgqVuO-2jxaLO6lC9w6w
+
+&expires_in=3600&token_type=Bearer
+
+
+
+https://mybucketnancy.s3.amazonaws.com/dragonsapp/index.html#/id_token=
+
+
+
+&expires_in=3600&token_type=Bearer
+
+
+https://mybucketnancy.s3.amazonaws.com/dragonsapp/index.html#/id_token=
+
+eyJraWQiOiJSeVdXNm5kYzZHQzlLSStpN1kwVnR6bHU2TEZJVisySHZ2QnlwRU94Ymk0PSIsImFsZyI6IlJTMjU2In0.eyJhdF9oYXNoIjoieUFGNzAtZFc2OHZWT2xfSTFuVUFudyIsInN1YiI6IjkyMGEzODNkLTVmN2MtNDRiNi05OWM1LWVlMTNmYzQ4NjZlMyIsImNvZ25pdG86Z3JvdXBzIjpbIm9uZSJdLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tXC91cy1lYXN0LTFfb1ZoME1nZ04yIiwiY29nbml0bzp1c2VybmFtZSI6Im56aGFuZzYiLCJjb2duaXRvOnJvbGVzIjpbImFybjphd3M6aWFtOjo5NzM3MjE5NDc3NjE6cm9sZVwvdGVzdC1oamRzZG5sc2FkYXNkLWFwaSJdLCJhdWQiOiI0czVlZmRsZm9xc241ZGlsbTgwZTd1aDk0MSIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNjYwNjg4ODIwLCJleHAiOjE2NjA2OTI0MjAsImlhdCI6MTY2MDY4ODgyMCwianRpIjoiN2VlYzZiODUtMmZjNi00M2E0LTkxNzctMTg5OTg2ZWZhYWRmIiwiZW1haWwiOiJuemhhbmc2QGRlbG9pdHRlLmNvLm56In0.Cux51k3CyBsFWiqESqavKdyHB9SrmFQSFVKdtK6kvu2koiUmPl26rSywY0jVzRjtiPR3nRwYJc8b4l9CqYF1mBl14cSgWBzvuGcMp6ZVY5vXgdxJTjAyHOiiM12KIAnVThy6JhyELLk3GVrbIqMZub6vcIbemMlT4b3MfJXUT7336cZfMULKBWKlvc2gZayvMmZquilUYj6Fn8n1kyCl5Sr7Kpr_SxPOZ5LRI9Dn2IVNtiI648Ojkcwte1KmcebyETSp8y4sgy4_QPU7WQD6XpdvGpdyK7UQEsSgpj0HFRFJwKvsthkrg0pnTeqGL9cLZ2_MMW1KoFuni_6lqtmiBw&access_token=eyJraWQiOiIrRXlcL2xDa1N6VlwvRW9aMEI3R3ZDV3Q0dGtyWjkxS3JDQTMxMmZrNzMyNmM9IiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiI5MjBhMzgzZC01ZjdjLTQ0YjYtOTljNS1lZTEzZmM0ODY2ZTMiLCJjb2duaXRvOmdyb3VwcyI6WyJvbmUiXSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tXC91cy1lYXN0LTFfb1ZoME1nZ04yIiwidmVyc2lvbiI6MiwiY2xpZW50X2lkIjoiNHM1ZWZkbGZvcXNuNWRpbG04MGU3dWg5NDEiLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwiYXV0aF90aW1lIjoxNjYwNjg4ODIwLCJleHAiOjE2NjA2OTI0MjAsImlhdCI6MTY2MDY4ODgyMCwianRpIjoiNmNiZDAwMmUtM2M3Zi00MjRiLTk3NjUtMWEzOGZmYjNjMTg4IiwidXNlcm5hbWUiOiJuemhhbmc2In0.IbVKv1CIJFHw2-pvrqtvpDDVpWcVYL7GXXcXb_9NaOz7BFUijsq-6Qp6Pkkvb21M2aUA-UNAXn0BPw8aeWf6cSTb0WQf-A87XjraFIp8SE7N0BvG75iLX9VhgCsvNVLxnEp0gNMr0ISbvrYIi5ZF2L8ewolOdYWnBfwGXW4c-JM7eoN44UlNirlJMis1vfdH_qiKeppvg-Ez3jWxtwhNZQ2tmEuv3ykhH-UcmmSvESx-1CxUHwIiMopaaQPmlx1VPaAjP3zbB2rJMIMNf53Oc0mMKg8k_bmZkysO_uoeZgMNzwk1eaATsbBbwIshkUj5U9mjEhXt-ANWXq6khn61Yw
+
+&expires_in=3600&token_type=Bearer
+
+MYDEMOBUCKET=nancydemobucket
+
+aws s3 cp ~/webapp3 s3://$MYBUCKET/dragonsapp/ --recursive --acl public-read
+
+aws s3 cp ~/webapp3 s3://$MYDEMOBUCKET/dragonsapp/ --recursive --acl public-read
+
+
+
+export MYBUCKET=$bucket
+
+export NANCYCOGNITOBUCKET="nancycognitobucket"
+
+aws s3 mb s3://$NANCYCOGNITOBUCKET
+
+echo "export NANCYCOGNITOBUCKET=$NANCYCOGNITOBUCKET" >> ~/.bashrc
+
+
+aws s3 cp ~/webapp3 s3://$MYBUCKET/dragonsapp/ --recursive --acl public-read
+
+aws s3 cp ~/webapp3 s3://$NANCYCOGNITOBUCKET/dragonsapp/ --recursive --acl public-read
+
+https://nancycognitobucket.s3.amazonaws.com/dragonsapp/index.html
+
+
+
+{
+     "description_str":"George is a new dragon, we don't know much about them yet.",
+     "dragon_name_str":"George",
+     "family_str":"green",
+     "location_city_str":"seattle",
+     "location_country_str":"usa",
+     "location_neighborhood_str":"4th st",
+     "location_state_str":"washington"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+1,2,3,
+
+
+
+
+
+
+Create a branch for ando dev
+Modify on it
+
+aws dev account? dev env???
+
+mule data source env? how many??? 
+
+target env,how many????
+
+Deploy on aws dev env, by python? By terraform, then local test
+Merge to ando, trigger deploy to ando stg
+If test ok, push to ando prod
+
+??????? study cloudhub api, study elastic search, kibana
+?????????delootte mulesoft, delotte elastic search account
+??????depoloy in sandbox first
+local docker for learning
+
+>?????? diff between lanmbda and non-lambda repo
+Build
+This project has a dependency on anypoint-api-client. 
+Ensure that you have configured your maven settings to use the DPE S3 maven repository. 
+The dependency is published on the DPE maven repo.
+
+？？？？？破坏sandbox原有的，build from 0 + 还原
+？？？？？ then pointing to opensearch instead of elastic search 
+
+？？？？？？docker compose run elastic serach 
+
+
+
+
+
+
+if x not xxx and x not yyy and x-y not zzz
+    res.append(x)
+
+1
+23
+32
+
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCTqlmzjBWqrh5FQOoK5B2AISUWVn/tSClcKfAcPKQeadsAdRdFvLi7Ji/bpJNQuze7fEKIXTx/9BspshyzPgpLbXftgrI726bqgILgHhFkICr6DOgvS5WEWz104AD2V+ED+v0AucI7ZAlyR8jS3y5WN5BnU0PY5wXdBTSavGMkA0zRBzvUr3gpFd2JeZwZk3+GjNxM04JmAmOJVxo44H9G4F2ijuqB1VEW1zAYe534pDrgoEAerFsONf4ZyBdo/IsuHxFNOgAbUDpnExqxQFX6hsigFL/gLVEJnUw+GR8JO5ImZ5EceCXlZvP1FOoa0n56OpyGDRePwNsGBBZaeQmeFCHD64QzZkPowybjkCqWulygzbv0OGW5LVVjyJHZS+KDlGcDtM2+O4jEzEz2ocIpXIpXcKczmn6jGAnK/QSKMqUTQYkbMLXKueylaIj/XcA1DyQbPjqQTKObqo1HgXelKhFyhELRfTJhLwuRI6j9+xKa2t3nxjT9OrESX166KL8= nzhang6@NZ-PN9DM9C4KY
